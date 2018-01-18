1b94181cd47a8fec9f9a720a81eecb233057b3cf
Jerry <>
2018-01-18 17:26:25 +0800

    daily commit

---
+++ app/code/local/PChomePay/PChomePayPayment/Model/PaymentModel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jerry
+ * Date: 2018/1/18
+ * Time: 下午4:09
+ */
+
+class PChomePay_PChomePayPayment_Model_PaymentModel extends Mage_Payment_Model_Method_Abstract
+{
+
+}
