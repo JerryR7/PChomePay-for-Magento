6bf99e7af3c99ca28d70381fa2090d3e9fcdb183
Jerry <>
2018-01-25 17:59:38 +0800

    daily commit

---
+++ app/code/local/PChomePay/PChomePayPayment/controllers/PaymentController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jerry
+ * Date: 2018/1/25
+ * Time: 上午11:03
+ */
+
+class PChomePay_PChomePayPayment_PaymentController extends Mage_Core_Controller_Front_Action
+{
+
+}
